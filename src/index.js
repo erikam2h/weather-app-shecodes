d3a743d370a34c1f343974a00d546e2245e68fe6
erikam2h <erikam.mh90@gmail.com>
2021-10-04 12:30:40 -0500

    Alt conected to Js to change with the searching

--- src/index.js
+++ src/index.js
@@ -69,6 +69,7 @@ function displayWeatherCondition(response) {
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
+  iconElement.setAttribute("alt", response.data.weather[0].main);
 }
 
 // city name
